651418dc7eb442e1251486d30e35365b140f658f
Christian Garbs <mitch@cgarbs.de>
2015-08-01 22:32:14 +0200

    first really usable example: list all known hosts

--- example.pl
+++ example.pl
@@ -19,7 +19,6 @@ if (-r $rcfile)
 	chomp $line;
 	if ($line =~ /^(\S+)\s*=\s*(.*?)$/)
 	{
-	    warn "<$1> -> <$2>\n";
 	    if ($1 eq 'username')
 	    {
 		$user = $2;
@@ -39,22 +38,65 @@ my $f = Fritz->new(
     password => $pass
     );
 
-$f->dump();
-print "\n\n";
+#$f->dump();
+#print "\n\n";
 
 my $d = $f->discover();
-# $d->dump();
-print "\n\n";
+#$d->dump();
+#print "\n\n";
 
-my $s = $d->find_service( 'urn:dslforum-org:service:WANDSLInterfaceConfig:1' );
-$s->dump();
-print "\n\n";
+if (1 == 0)
+{
+    # get all connected WLAN devices
+    my $services = $d->find_service_names('Configuration:');
+    $services->errorcheck;
+    print Dumper($services->data);
+    for my $serviceType (@{$services->data})
+    {
+	my $service = $d->get_service($serviceType);
+	$services->errorcheck;
+	my $response = $service->call('GetTotalAssociations');
+	$response->errorcheck;
+	print Dumper($response->data);
+	# now do a loop and read device by device - my WLAN is off, so I can't test here :-)
+    }
+}
 
-$s = $d->find_service( Fritz::Service::DEVICEINFO );
-$s->dump();
-print "\n\n";
+if (1 == 1)
+{
+    ## list all known hosts
+    my $service = $d->find_service('Hosts:1');
+    $service->errorcheck;
+    my $response = $service->call('GetHostNumberOfEntries');
+    $response->errorcheck;
+    printf "number of hosts = %d\n", $response->data->{NewHostNumberOfEntries};
+    printf "%3s  %-30s  %-15s  %-17s  %-10s  %-6s  %s\n", 'act', 'hostname', 'IP address', 'MAC address', 'interface', 'source', 'lease';
+    for my $host ( 0 .. $response->data->{NewHostNumberOfEntries} - 1)
+    {
+	$response = $service->call('GetGenericHostEntry', 'NewIndex' => $host);
+	$response->errorcheck;
+	my $d = $response->data;
+	printf("%3d  %-30s  %-15s  %-17s  %-10s  %-6s  %d\n",
+	       $d->{NewActive},
+	       $d->{NewHostName},
+	       $d->{NewIPAddress},
+	       $d->{NewMACAddress},
+	       $d->{NewInterfaceType},
+	       $d->{NewAddressSource},
+	       $d->{NewLeaseTimeRemaining},
+	    );
+    }
+}
 
-my $r;
+#my $s = $d->find_service( 'urn:dslforum-org:service:WANDSLInterfaceConfig:1' );
+#$s->dump();
+#print "\n\n";
+
+#$s = $d->find_service( Fritz::Service::DEVICEINFO );
+#$s->dump();
+#print "\n\n";
+
+#my $r;
 
 #print Dumper( $s->scpd() );
 
@@ -62,9 +104,9 @@ my $r;
 #$r->dump();
 #print "the security port is " . $r->data->{'NewSecurityPort'} . "\n\n";
 
-$r = $s->call('GetInfo');
+#$r = $s->call('GetInfo');
 
-print Dumper($r);
+#print Dumper($r);
 
 #$s = $d->find_service( 'urn:dslforum-org:service:X_VoIP:1' );
 #print Dumper($s->dump);
